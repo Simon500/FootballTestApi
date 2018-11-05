cfebed1258ef34a06d68e4d812a708dfade360e6
Simon Mutie <simihmutie@gmail.com>
2018-11-05 14:46:03 +0300

    Added Procfile and requirements

--- footballtestapi/settings.py
+++ footballtestapi/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'footballtestapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
